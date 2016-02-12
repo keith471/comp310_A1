9fb74dd26da3f50c46eba4e98c366e88e7f11882
keith471 <keithstrickling@gmail.com>
2016-02-12 17:55:50 -0500

    Intense history update complete

--- oscode.c
+++ oscode.c
@@ -5,11 +5,16 @@
 #include <errno.h>
 #include <fcntl.h>
 #include <signal.h>
+#include <sys/wait.h>
+#include <sys/mman.h>
 #include "helpers.h"
 #include "list.h"
 
 const int HISTORY_SIZE = 10;
+const int ERR_CMDS_SIZE = 20;
 const int MAX_ARGS = 20;
+const int EXECVP_FAIL = 100;
+const int EXECVP_UNDEF_ERR = 101;
 
 typedef enum {HISTORY, HISTACCESS, PWD, CD, JOBS, EXIT, FG, EXTERNAL} cmd_type;
 
@@ -24,7 +29,11 @@ typedef struct {
 } cmdTuple;
 
 cmdTuple history[HISTORY_SIZE];
+int err_cmds[ERR_CMDS_SIZE] = {0};
+int errcount = 0;
 int cmdcount = 1;
+static int *erroneous_cmd_num;	// A shared variable
+
 
 int getcmd(char *prompt, char *args[], int *background, cmd_type *type, int *redirectIdx) {
     int length, i = 0;
@@ -103,20 +112,60 @@ cmdTuple* getHistory(int cmd) {
     return NULL;
 }
 
-void addToHistory(int cmdnum, int err, cmd_type type, int bg, int redirectIdx, char **args, int numargs) {
-    int insertIndex = (cmdnum - 1) % 10;
+int updateHistory(int cmdnum) {
+    printf("Updating history\n");
+    for (int i = 0; i < HISTORY_SIZE; i++) {
+        if (history[i].cmdnum == cmdnum) {
+            history[i].error = 1;
+            return 1;
+        }
+    }
+    
+    return 0;
+}
+
+void printHistory() {
+	int i = 0;
+	while ((history[i].args[0] != NULL) && i < 10) {
+		printf("(%i) ", history[i].cmdnum);
+		printCommand(history[i].args, history[i].numargs);
+		i++;
+	}
+	if (i == 0) {
+		printf("Nothing in history\n");
+	}
+}
+
+int erroneousCmd(int cmdnum) {
+    for (int i = 0; i < ERR_CMDS_SIZE; i++) {
+        if (err_cmds[i] == cmdnum) {
+            return 1;
+        }
+    }
+    return 0;
+}
+
+void addToHistory(int cmdnum, cmd_type type, int bg, int redirectIdx, char **args, int numargs) {
+    int insertIndex = (cmdnum - 1) % HISTORY_SIZE;
     cmdTuple hist;
     hist.cmdnum = cmdnum;
+    int err = 0;
+    if (erroneousCmd(cmdnum)) {
+        err = 1;
+    }
     hist.error = err;
     hist.type = type;
     hist.bg = bg;
     hist.redirectIdx = redirectIdx;
-    // sizeof(args) will give you the size of the pointer. This is no good to us, so instead
-    // we use sizeof(args[0]) * numargs
     memcpy(hist.args, args, numargs*sizeof(args[0]));
     hist.args[numargs] = NULL;	// null-terminate args array
     hist.numargs = numargs;
     history[insertIndex] = hist;
+}
+
+void addToErrCmds(int cmdnum) {
+    int insertIndex = errcount % ERR_CMDS_SIZE;
+    err_cmds[insertIndex] = cmdnum;
 }
 
 void addToJobs(pid_t pid, char **args, int numargs) {
@@ -132,6 +181,8 @@ int runCmd(char *args[], int numargs, int cmdcount, int bg, int redirectIdx) {
 	if (childPID < 0) {
 		fprintf(stderr, "The fork failed");
 	} else if (childPID == 0) {
+	    printf("Parent pid: %d\n", getppid());
+	    printf("Child pid: %d\n", getpid());
 		// Child process
 		// If redirectIdx != -1, then the output of all the commands should go elsewhere
 		if (redirectIdx != -1) {
@@ -150,40 +201,59 @@ int runCmd(char *args[], int numargs, int cmdcount, int bg, int redirectIdx) {
 			// dup again to rewire stdin
 			err = execvp(newargs[0], newargs);
 			
-			if (err == -1) {
-		        printf("Error: %s\n", strerror(errno));
-		        printf("Exiting child process...\n");
-		        return err;
-		        exit(11);
-		    }
+			printf("Error: %s\n", strerror(errno));
+		    printf("Exiting child process...\n");
+			
+			if (!bg) {
+			    if (err == -1) {
+		            exit(EXECVP_FAIL);
+		        } else {
+		            exit(EXECVP_UNDEF_ERR);
+		        }
+			} else {
+			    *erroneous_cmd_num = cmdcount;
+			    kill(getppid(), SIGUSR1);
+			    exit(EXIT_FAILURE);
+			}	    
 		    
 		} else {
 		    err = execvp(args[0], args);
 		    
-		    if (err == -1) {
-		        printf("Error: %s\n", strerror(errno));
-		        printf("Exiting child process...\n");
-		        exit(11);
-		    }
+		    printf("Error: %s\n", strerror(errno));
+		    printf("Exiting child process...\n");
+		    
+		    if (!bg) {
+			    if (err == -1) {
+		            exit(EXECVP_FAIL);
+		        } else {
+		            exit(EXECVP_UNDEF_ERR);
+		        }
+			} else {
+			    *erroneous_cmd_num = cmdcount;
+			    kill(getppid(), SIGUSR1);
+			    exit(EXIT_FAILURE);
+			}
+
 		}
 	} else {
 		// Parent process
 		if (!bg) {
 			int status = 0;
 			waitpid(childPID, &status, 0);
-			if (status == 11) {
-			    return -1;
+			//printf("Returned child status: %d\n", status);
+			int exit_status = WEXITSTATUS(status);
+			printf("Exit STATUS %i\n", exit_status);
+			if (exit_status == EXECVP_FAIL || exit_status == EXECVP_UNDEF_ERR) {
+			    return 1;
 			} else {
 			    return 0;
 			}
 		} else {
 		    printf("Process ID: %i\n", childPID);
 		    addToJobs(childPID, args, numargs);
+		    return 0;
 		}
 	}
-	
-	return err;
-
 }
 
 // Return values:
@@ -197,13 +267,7 @@ void processCommand(char **args, int numargs, cmd_type type, int bg, int redirec
     
     if (type == HISTORY) {
         // Loop through history and print the number and command
-        int i = 0;
-        while ((history[i].args[0] != NULL) && i < 10) {
-            printf("(%i) ", history[i].cmdnum);
-            printCommand(history[i].args, history[i].numargs);
-            i++;
-        }
-        
+        printHistory();
     } else if (type == HISTACCESS) {
         // Look for the command in history
 		cmdTuple *cmd = getHistory(atoi(args[0]));
@@ -310,9 +374,10 @@ void processCommand(char **args, int numargs, cmd_type type, int bg, int redirec
     } else if (type == EXIT) {
         exit(EXIT_SUCCESS);
     } else if (type == EXTERNAL) {
-    
-        int result = runCmd(args, numargs, cmdcount, bg, redirectIdx);
-        if (result == -1) {
+        int pid = 0;
+        int err = runCmd(args, numargs, cmdcount, bg, redirectIdx);
+        printf("After runcmd\n");
+        if (err) {
             saveToHistory = 0;
         }
     }
@@ -320,8 +385,7 @@ void processCommand(char **args, int numargs, cmd_type type, int bg, int redirec
     // Add the command to history
     if (saveToHistory) {
         printf("Saving to history: %s\n", args[0]);
-        addToHistory(cmdcount, 0, type, bg, redirectIdx, args, numargs);
-
+        addToHistory(cmdcount, type, bg, redirectIdx, args, numargs);
         cmdcount++;
     }
     
@@ -342,6 +406,63 @@ void handle_SIGINT(int sig) {
     printf("To exit, use 'exit'\n");
 }
 
+/*void handle_SIGCHLD(int sig) {
+    signal(SIGCHLD, handle_SIGCHLD);
+    // sig seems to always be 20, no matter the reason for the signal
+    pid_t pid;
+    int status = 0;
+  	pid = wait(&status);	// Get the pid of the process issuing the status
+  	printf("\nPid %d exited.\n", pid);
+
+  	
+  	if (WIFEXITED(status)) {
+  	    // Child process terminated normally with a call to exit(status_code)
+  	    int exit_status = WEXITSTATUS(status);
+  	    printf("\nChild exited normally with status %u\n", exit_status);
+  	    if (exit_status == EXECVP_FAIL || exit_status == WEXITSTATUSEXECVP_UNDEF_ERR) {
+  	        // If process with PID pid has already been added to history, then update its
+  	        // err field. Otherwise, place pid in err_pids so that when the process is added
+  	        // to history, it's err field can be appropriately set
+  	        if (!updateHistory(pid)) {
+  	            addToErrPids(pid);
+  	        }
+  	    }
+  	} else if (WIFSIGNALED(status)) {
+  	    // Child process terminated because it received a signal that was not handled
+  	    printf("\nChild received signal %u, causing abnormal termination\n", WTERMSIG(status));  	
+  	} else if(WIFSTOPPED(status)) {
+  		// Child was stopped by a signal
+  	    printf("\nChild stopped by signal %u\n", WSTOPSIG(status));
+  	} else if (WCOREDUMP(status)) {
+  		// Child terminated and produced a core dump
+  		printf("\nChild terminated and produced a core dump\n");
+  	}
+  	
+}*/
+
+void handle_SIGUSR1(int sig) {
+    signal(SIGUSR1, handle_SIGUSR1);
+    
+    // Get the command number of the command that failed
+    int failedcmd = *erroneous_cmd_num;
+    
+    printf("Failed cmd num: %i\n", failedcmd);
+    
+    // Check if the command has already been added to history.
+    // If so, update its err field.
+    // Else, add it to err_cmds so that when the command is added to history, its err field
+    // can be appropriately set
+    if (!updateHistory(failedcmd)) {
+        printf("Adding to erroneous commands: %i failed cmd num\n", failedcmd);
+        addToErrCmds(failedcmd);
+    }
+
+}
+
+void cleanup() {
+	munmap(erroneous_cmd_num, sizeof(*erroneous_cmd_num));
+}
+
 int main() {
 
     char *args[20];
@@ -353,9 +474,17 @@ int main() {
     
     //void handle_SIGTSTP(int);
     void handle_SIGINT(int);
+    //void handle_SIGCHLD(int);
+    void handle_SIGUSR1(int);
     
     //signal(SIGTSTP, handle_SIGTSTP);	// Receive and handle CTRL-Z
     signal(SIGINT, handle_SIGINT);		// Receive and handle CTRL-C
+    //signal(SIGCHLD, handle_SIGCHLD);
+    signal(SIGUSR1, handle_SIGUSR1);
+    
+    erroneous_cmd_num = mmap(NULL, sizeof(*erroneous_cmd_num), PROT_READ | PROT_WRITE, MAP_SHARED | MAP_ANONYMOUS, -1, 0);
+    
+    *erroneous_cmd_num = 0;
 
     while (1) {
         
@@ -379,6 +508,8 @@ int main() {
 		//freecmd(args, cnt);
 				 
 	}
+	
+	cleanup();
 		
 	return 0;
      
